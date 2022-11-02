d6ee53ff678e0d3e1079bb6e2288c131edbeb817
Isabella Aguilar <ia514648@gmail.com>
2022-11-02 00:14:15 -0600

    cambiar nombre a revision de PDF

--- src/layouts/pages/landing-pages/formsadmin/solicitudespendientesNFT.js
+++ src/layouts/pages/landing-pages/formsadmin/solicitudespendientesNFT.js
@@ -24,7 +24,7 @@ import CloseIcon from "@mui/icons-material/Close";
 function downloadPDF(pdf) {
     const linkSource = `data:application/pdf;base64,${pdf}`;
     const downloadLink = document.createElement("a");
-    const fileName = "pdf.pdf";
+    const fileName = "revision.pdf";
     downloadLink.href = linkSource;
     downloadLink.download = fileName;
     downloadLink.click();
